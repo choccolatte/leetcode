b873ec423333687bcc8b087ec8ee50d8e85cf33a
A Sin <124335907+choccolatte@users.noreply.github.com>
2026-02-09 21:07:12 +0530

    contains duplicate using C# - v0902262107

--- containsDup.py
+++ containsDup.py
@@ -16,3 +16,18 @@ class Solution2:
 			if nums[i] == nums[i-1]:
 				return True
 		return False
+
+#in C#
+# public class Solution{
+# 	public bool hasDup(int[] nums){
+# 		for(int i = 0; i < nums.Length; i++){
+# 			for(int j = i + 1; j < nums.Length; j++){
+# 				if(nums[i] == nums[j]){
+# 					return true;
+# 				}
+# 			}
+# 		}
+
+# 		return false;
+# 	}
+# }
